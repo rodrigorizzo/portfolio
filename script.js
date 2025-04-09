3b3d87d689dafd7e8afb8a62009f0ebcb3517ab1
Rodrigo Rizzo <38816338+rodrigorizzo@users.noreply.github.com>
2025-04-09 00:10:17 -0300

    Melhora na responsividade da página e implementação de menu mobile

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const projH2 = document.querySelectorAll('#projetos h3');
-const YSobre = document.querySelector('#sobre').offsetTop;
-const sobre = document.querySelector('#sobre p');
+const dropDown = document.getElementById("mobileMenu");
 
-console.log("Ysobre="+ YSobre);
+let dropDownOn = false;
 
-window.addEventListener('scroll', scrollTeste, false);
+function abrirMenu() {
+    if (!dropDownOn) {
+        dropDown.classList.add("mostrar");
+        dropDownOn = true;
+    } else {
+        fecharMenu();
+        dropDownOn = false
+    }
 
+}
 
-function scrollTeste() {
-    console.log(window.pageYOffset);
-    if (YSobre > window.pageYOffset && window.pageYOffset < YSobre + 60) {
-        sobre.setAttribute('style', 'display: block;'); 
-    }else{
-        sobre.setAttribute('style', 'display: none;');
+function fecharMenu() {
+    dropDown.classList.remove("mostrar")
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches("#botaoMenu")) {
+        fecharMenu();
     }
 }
